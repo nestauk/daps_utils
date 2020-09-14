d36bd2d479e7b13ec7eed9bb2a2186cb551b87f7
Joel Klinger <joel.alexander.klinger@gmail.com>
2020-09-14 12:18:48 +0100

    copies flow dir

--- daps_utils/__init__.py
+++ daps_utils/__init__.py
@@ -1,6 +1,6 @@
 from .__initplus__ import path_to_init, __basedir__, load_config
 try:
-    from .breadcrumbs import drop_breadcrumb
+    from .breadcrumbs import drop_breadcrumb as talk_to_luigi
     from .tasks import _MetaflowTask, MetaflowTask
     config = load_config()
 except ModuleNotFoundError:  # For integration with setup.py
