efde1f6da44b85d504b1802293132d07fe184d3e
Joel Klinger <joel.alexander.klinger@gmail.com>
2020-09-15 15:04:05 +0100

    added script unit tests

--- .travis/test_scripts.sh
+++ .travis/test_scripts.sh
@@ -1,18 +1,29 @@
 #!/bin/bash
 
 set -e
-set -x
 
 function SetupDummyDirectory(){
+    # Set up directory ready for script testing    
     mkdir ${1}/script-tests &> /dev/null || true
     cd ${1}/script-tests
-    mkdir ${2}/${2}
+    mkdir -p ${2}/${2}    
     cd ${2}
+    git init .
+    git commit --allow-empty -m 'dummy commit'
 }
 
-function RunScriptTests(){
-    bash ${1}/scripts/${2}
-    bash ${1}/scripts/tests/${2}
+function RunScript(){
+    # Try running the script without and with args
+    ${2} || ${2} ${2}
+}
+
+function ScriptIsInstalled(){
+    # Check the script is installed
+    if ! command -v ${1} &> /dev/null
+    then
+	echo "Command '${1}' does not exist. Check setup.py to understand why it hasn't been installed." >&2
+	exit 1
+    fi
 }
 
 # Start
@@ -20,7 +31,16 @@ TOPDIR=${PWD}
 
 # For each script, create a dummy directory,
 # setup git and run the script
-for filename in ${TOPDIR}/scripts/*; do
+for filepath in ${TOPDIR}/scripts/*; do
+    filename=$(basename $filepath)
+    echo "#################################################"
+    echo -e "###### Running test for ${filename} ######\n" 
+    set -x
+    ScriptIsInstalled ${filename}
     SetupDummyDirectory ${TOPDIR} ${filename}
-    RunScriptTests ${TOPDIR} ${filename}
+    RunScript ${TOPDIR} ${filename}
+    rm -rf ${1}/script-tests
+    set +x    
+    echo -e "\n############ All tests passed #################"
+    echo -e "###############################################\n"
 done
