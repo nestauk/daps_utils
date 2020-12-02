4e07cc356274f6fa91f94420a6a9ed45e3ecbc8e
Joel Klinger <joel.alexander.klinger@gmail.com>
2020-12-02 13:05:49 +0000

    added docs to tests

--- daps_utils/tests/test_tasks.py
+++ daps_utils/tests/test_tasks.py
@@ -1,11 +1,27 @@
+"""
+test_tasks
+==========
+
+Tests for tasks.py.
+"""
+
 from unittest import mock
 
 from daps_utils.tasks import luigi, ForceableTask
+
+# Note that toggle_force_to_false, toggle_exists are
+# tested under the ForceableTask tests, since it's very hard
+# to write transparent tests for class method decorators
+#from daps_utils.tasks import toggle_force_to_false, toggle_exists
+
 
 PATH = "daps_utils.tasks.{}"
 
 
 class MockTarget(mock.MagicMock):
+    """Need to define a concrete 'exists' method so that
+    the __class__ also has the 'exists' method to fall
+    back on when toggle_force_to_false is called"""
     def exists(self):
         return True
 
@@ -43,6 +59,8 @@ class MockTask(ForceableTask):
 @mock.patch(PATH.format("luigi.task.flatten"),
             side_effect=lambda x: [x])
 def test_run_affects(mock_flatten):
+    """Test that output() will toggle exists() to False,
+    and then run() toggles it back"""
     task = MockTask(name='abc', force=True)
     # Originally the output actually exists
     assert task.out.exists() is True
@@ -56,6 +74,7 @@ def test_run_affects(mock_flatten):
 @mock.patch(PATH.format("luigi.task.flatten"),
             side_effect=lambda x: [x])
 def test_run_no_force_no_effect(mock_flatten):
+    """Test that no toggling takes place when force is False"""
     task = MockTask(name='def', force=False)
     # Originally the output actually exists
     assert task.out.exists() is True
@@ -68,7 +87,9 @@ def test_run_no_force_no_effect(mock_flatten):
 
 @mock.patch(PATH.format("luigi.task.flatten"),
             side_effect=lambda x: [x])
-def test_trigger_output_affects(mock_flatten):
+def test_trigger_event_affects(mock_flatten):
+    """Test that output() will toggle exists() to False,
+    and then trigger_event() toggles it back"""
     task = MockTask(name='ghi', force=True)
     # Originally the output actually exists
     assert task.out.exists() is True
@@ -82,6 +103,8 @@ def test_trigger_output_affects(mock_flatten):
 @mock.patch(PATH.format("luigi.task.flatten"),
             side_effect=lambda x: [x])
 def test_other_function_doesnt_affect(mock_flatten):
+    """Test that output() will toggle exists() to False,
+    and then other_function() DOESN'T toggle it back"""
     task = MockTask(name='jkl', force=True)
     # Originally the output actually exists
     assert task.out.exists() is True
@@ -95,6 +118,9 @@ def test_other_function_doesnt_affect(mock_flatten):
 @mock.patch(PATH.format("luigi.task.flatten"),
             side_effect=lambda x: [x] if isinstance(x, MockTarget) else x)
 def test_force_upstream(mock_flatten):
+    """Test that output() will toggle exists() to False,
+    and then run() toggles it back, for all children also when
+    force_upstream is True"""
     task = MockTask(name='mno', force_upstream=True)
     children = list(task.requires())
     assert len(children) == 3
