382e05ac2b3f5eafcbbcc361eb6c768a0753a34c
Joel Klinger <joel.alexander.klinger@gmail.com>
2021-02-16 13:37:18 +0000

    updated tests

--- daps_utils/tests/test_flow.py
+++ daps_utils/tests/test_flow.py
@@ -1,3 +1,5 @@
+from unittest import mock
+
 from daps_utils import DapsFlowMixin
 
 
@@ -17,3 +19,32 @@ def test_modes_production():
     flow = MyFlow()
     assert flow.production
     assert not flow.test
+
+
+def test_db_test_actual_test():
+    class MyFlow(DapsFlowMixin):
+        pass
+    for value in (True, False):
+        MyFlow.production = value
+        flow = MyFlow()
+        assert flow.db_name == 'test'
+
+
+@mock.patch('daps_utils.flow.sys')
+def test_db_test_override_sys_production(mocked_sys):
+    mocked_sys.modules = []
+    class MyFlow(DapsFlowMixin):
+        pass
+    MyFlow.production = True
+    flow = MyFlow()
+    assert flow.db_name == 'production'
+
+
+@mock.patch('daps_utils.flow.sys')
+def test_db_test_override_sys_dev(mocked_sys):
+    mocked_sys.modules = []
+    class MyFlow(DapsFlowMixin):
+        pass
+    MyFlow.production = False
+    flow = MyFlow()
+    assert flow.db_name == 'dev'
