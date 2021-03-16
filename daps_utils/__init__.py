0e5a4bdc1e37e43404a08700a0232680df186466
bishax <alex.bishop@nesta.org.uk>
2021-03-16 13:58:34 +0000

    Proposed fix

--- daps_utils/__init__.py
+++ daps_utils/__init__.py
@@ -1,13 +1,17 @@
+import os
+
 from .__initplus__ import __basedir__, load_config, load_current_version
 
-try:
-    from .breadcrumbs import drop_breadcrumb as talk_to_luigi
-    from .tasks import (MetaflowTask, CurateTask, ForceableTask,
-                        DapsTaskMixin, DapsRootTask)
-    from .flow import DapsFlowMixin
-    config = load_config()
-except ModuleNotFoundError as exc:  # For integration with setup.py
-    print(exc)
-    pass
-
 __version__ = load_current_version()
+
+if "IN_SETUP" not in os.environ:
+    try:
+        from .breadcrumbs import drop_breadcrumb as talk_to_luigi
+        from .tasks import (MetaflowTask, CurateTask, ForceableTask,
+                            DapsTaskMixin, DapsRootTask)
+        from .flow import DapsFlowMixin
+        config = load_config()
+    except ModuleNotFoundError as exc:  # For integration with setup.py
+        print(exc)
+        pass
+
