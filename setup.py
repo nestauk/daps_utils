6730c3d70f22eecd7b75e88415e0385d5c780dd7
Johan Carlin <johan.carlin@gmail.com>
2021-09-29 15:45:11 +0100

    add project_name.py to scripts to unbreak CI, run black while we're at it

--- setup.py
+++ setup.py
@@ -7,35 +7,41 @@ os.environ["IN_SETUP"] = "1"  # noqa: E402
 from daps_utils import __version__, __basedir__  # noqa: E402
 
 
-version = ''.join(v for v in __version__ if (v.isnumeric() or v == '.'))
+version = "".join(v for v in __version__ if (v.isnumeric() or v == "."))
 
-with open(f'{__basedir__}/requirements.txt') as f:
+with open(f"{__basedir__}/requirements.txt") as f:
     required = f.read().splitlines()
 
-exclude = ['docs', 'tests*']
+exclude = ["docs", "tests*"]
 common_kwargs = dict(
     version=version,
-    license='MIT',
+    license="MIT",
     install_requires=required,
-    long_description=open('README.md').read(),
-    url='https://github.com/nestauk/daps_utils',
-    author='nesta',
-    author_email='software_development@nesta.org.uk',
-    maintainer='nesta',
-    maintainer_email='software_development@nesta.org.uk',
+    long_description=open("README.md").read(),
+    url="https://github.com/nestauk/daps_utils",
+    author="nesta",
+    author_email="software_development@nesta.org.uk",
+    maintainer="nesta",
+    maintainer_email="software_development@nesta.org.uk",
     classifiers=[
-        'Development Status :: 1 - Planning',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Natural Language :: English',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3.7',
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3.7",
     ],
-    python_requires='>=3.7',
+    python_requires=">=3.7",
     include_package_data=True,
 )
 
-setup(name='daps_utils',
-      packages=find_namespace_packages(where='.', exclude=exclude),
-      scripts=['scripts/metaflowtask-init', 'scripts/calver-init'],
-      **common_kwargs)
+setup(
+    name="daps_utils",
+    packages=find_namespace_packages(where=".", exclude=exclude),
+    scripts=[
+        "scripts/metaflowtask-init",
+        "scripts/calver-init",
+        "scripts/project_name.py",
+    ],
+    **common_kwargs,
+)
